56a6a6600262c3cd008647e86f28591fa9668910
Denis.Karev <goofy.adun@gmail.com>
2022-12-12 16:34:34 +0300

    some w

--- HW4/HW4.java
+++ HW4/HW4.java
@@ -1,29 +1,62 @@
 package HW4;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class HW4 {
-    public static void printMap(int[][] mapa) {
-        StringBuffer top = new StringBuffer("▒▒");
-        for (int i = 0; i < mapa[0].length+1; i++) {
-            top.append("▒▒");
+
+    public static int[] getToEndPoint(int[][] mapa, int[] start) {
+        int[] finish = new int[] { -1, -1 };
+        System.out.println(mapa.length);
+        System.out.println(mapa[0].length);
+        // System.out.printf("x %d, y %d\n", start[0], start[1]); // print start
+        Queue<int[]> pathCalc = new LinkedList<int[]>();
+        pathCalc.add(start);
+        mapa[start[1]][start[0]] = 1;
+        printMap(mapa);
+        int[] tempc = new int[] { 0, 0 };
+        while (pathCalc.size() != 0) {
+            int[] point = pathCalc.poll();
+            int x = point[1];
+            int y = point[0];
+            System.out.printf("x %d, y %d\n", point[0], point[1]);
+
+            // if (x < 0 || y < 0)
+            // System.err.println("x or y < 0. Error!");
+
+            if (mapa[y][x + 1] == 0) {
+                mapa[y][x + 1] = mapa[y][x] + 1;
+                pathCalc.add(new int[] { y, x + 1 });
+            }
+            if (mapa[y][x - 1] == 0) {
+                mapa[y][x - 1] = mapa[y][x] + 1;
+                pathCalc.add(new int[] { y, x - 1 });
+            }
+            if (mapa[y + 1][x] == 0) {
+                mapa[y + 1][x] = mapa[y][x] + 1;
+                pathCalc.add(new int[] { y + 1, x });
+            }
+            if (mapa[y - 1][x] == 0) {
+                mapa[y - 1][x] = mapa[y][x] + 1;
+                pathCalc.add(new int[] { y - 1, x });
+            }
+            printMap(mapa);
         }
-        System.out.println(top);
-        for (int[] is : mapa) {
-            printMapLine(is);
-        }
-        System.out.println(top);
+        return finish;
     }
-    
-    public static void printMapLine(int[] array) {
-        String wall = "▒▒";           // "█▓▒░"
-        // System.out.printf("[ ");
-        System.out.print(wall);
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == -1) {
-                System.out.print("▓▓");
-            } else System.out.printf("%2d", array[i]);
+
+    public static void printMap(int[][] mapa) {
+        // String wall = "▒▒"; // "█▓▒░"
+        for (int[] is : mapa) {
+            for (int i = 0; i < is.length; i++) {
+                if (is[i] == -1)
+                    System.out.print("▓▓");
+                else if (is[i] == -2)
+                    System.out.print("▒▒");
+                else
+                    System.out.printf("%2d", is[i]);
+            }
+            System.out.printf("\n");
         }
-        // System.out.printf("%2d", array[array.length - 1]);
-        System.out.print(wall);
-        System.out.printf("\n");
     }
 }
